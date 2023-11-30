48777bbae870449cfb141364bae101d2b0f7d914
Abdus samad <abdussamadkotha@gmail.com>
2023-11-30 19:47:44 +0500

    Basic Layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500'>Hello World</h1>
+    <h1 className='font-bold text-teal-500 text-2xl'>Hello World</h1>
   )
 }
