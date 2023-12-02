5ec946f278a6f26f35a0e0975c0d5380ecee5743
Abdus samad <abdussamadkotha@gmail.com>
2023-12-02 12:04:59 +0500

    Camp section created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-14 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
       <div
         className='relative z-20 flex flex-1 flex-col xl:w-1/2'
